108a97a773ca71032d304a8b091e861c07853935
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-04-21 17:23:54 -0300

    Calculadora 2023  - primeira versão da atividade

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -1,212 +1,116 @@
-﻿using System;
+﻿/**Requisito 01
+    Nossa calculadora deve ter a possibilidade de somar dois números [v]
+*/
+/**Requisito 02
+    Nossa calculadora deve ter a possibilidade fazer várias operações de soma [v]
+*/
+/**Requisito 03
+    Nossa calculadora deve ter a possibilidade fazer várias operações de soma e de subtração [v]
+*/
+/**Requisito 04
+    Nossa calculadora deve ter a possibilidade fazer as quatro operações básicas da matemática [v]
+*/
+/**Requisito 05
+    Nossa calculadora deve realizar as operações com "0" [v]
+*/
+/**Requisito 06
+    Nossa calculadora deve validar a opções do menu [v]
+*/
+/**Requisito 07
+    Nossa calculadora deve realizar as operações com números com duas casas decimais [v]
+*/
+/**Requisito 08
+    Nossa calculadora deve gerar a tabuada de multiplicação para um determinado número e apresentar numa visualização "rubro-negra" [v]
+*/
+/** Requisito 09
+Nossa calculadora deve permitir visualizar as operações já realizadas [v]
+* 
+*  Critérios:
+*      1 - A descrição da operação realizada deve aparecer assim, exemplo:
+*          2 + 2 = 4
+*          10 - 5 = 5
+*/
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Calculadora
+namespace Calculadora2023
 {
-    class Program 
-    {       
-            //escopo global
-            static string[] descricoesOperacao = new string[100]; //declaração e inicialização de arrays
-            static int quantidadeOperacoesRealizadas = 0;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "========== CALCULADORA ==========";
+            int larguraTela = Console.WindowWidth;
+            int larguraTexto = texto.Length;
+            int espacos = (larguraTela / 2) + (larguraTexto / 2);
+            Console.WriteLine(texto.PadLeft(espacos));
 
-            static void Main(string[] args)
-            {
-                while (true)
-                {
-                    string opcao = MostrarMenu();
+            double numero1, numero2, resultadoOperacao;
 
-                    if (opcao == "S" || opcao == "s")
-                        break;
+            Console.WriteLine("Informe o primeiro número: ");
+            numero1 = double.Parse(Console.ReadLine());
 
-                    if (OpcaoInvalida(opcao))
-                        ApresentarMensagem("Opção inválida, tente novamente...", "ERRO");
+            Console.WriteLine("Informe o segundo número: ");
+            numero2 = double.Parse(Console.ReadLine());
 
-                    else if (opcao == "5")
-                        GerarTabuada();
+            #region implementação do menu
+                Console.WriteLine("(\nSelecione a operação desejada:\")");
+                Console.WriteLine("1 - Somar dois números");
+                Console.WriteLine("2 - Fazer várias operações de soma");
+                Console.WriteLine("3 - Fazer subtração");
+                Console.WriteLine("3 - Fazer multiplicação");
+                Console.WriteLine("5 - Fazer divisao");
+                Console.WriteLine("6 - Visualizar as operações já realizadas");
+                Console.WriteLine("0 - Sair");
 
-                    else if (opcao == "6")
-                        VisualizarOperacoesRealizadas();
-
-                    else
-                        RealizarOperacao(opcao);
-                }
-            }
-
-            #region funções da calculadora
-            static string MostrarMenu()
-            {
-                Console.Clear();
-
-                // Defina as cores do console
-                Console.BackgroundColor = ConsoleColor.DarkBlue;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Clear();
-
-                // Imprime o cabeçalho personalizado
-                Console.WriteLine("+=========================================+");
-                Console.WriteLine("|                                         |");
-                Console.WriteLine("|             CALCULADORA C#              |");
-                Console.WriteLine("|                                         |");
-                Console.WriteLine("+=========================================+\n");
-
-                Console.WriteLine("Digite 1 para Adicionar");
-                Console.WriteLine("Digite 2 para Subtrair");
-                Console.WriteLine("Digite 3 para Multiplicar");
-                Console.WriteLine("Digite 4 para Dividir");
-                Console.WriteLine("Digite 5 para Gerar Tabuada");
-                Console.WriteLine("Digite 6 para Visualizar as operações realizadas");
-
-                Console.WriteLine("Digite S para sair");
-
-                string operacao = Console.ReadLine();
-
-                return operacao;
-            }
-
-            static bool OpcaoInvalida(string opcao)
-            {
-                bool opcaoInvalida =
-                    opcao != "1" &&
-                    opcao != "2" &&
-                    opcao != "3" &&
-                    opcao != "4" &&
-                    opcao != "5" &&
-                    opcao != "6" &&
-                    opcao != "S" &&
-                    opcao != "s";
-
-                return opcaoInvalida;
-            }
-
-            static void ApresentarMensagem(string mensagem, string tipo)
-            {
-                if (tipo == "ERRO")
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                Console.WriteLine(mensagem);
-                Console.ReadLine();
-                Console.ResetColor();
-            }
-
-            static void GerarTabuada()
-            {
-                Console.Write("Digite o número para gerar a tabuada: ");
-
-                int tabuada = Convert.ToInt32(Console.ReadLine()); //5              
-
-                for (int i = 1; i <= 10; i++) //i = i + 1 
-                {
-                    int resto = i % 2;
-                    if (resto == 0)
-                        Console.BackgroundColor = ConsoleColor.Red;
-                    else
-                        Console.BackgroundColor = ConsoleColor.Black;
-
-                    int resultadoMultiplicacao = tabuada * i;
-
-                    Console.WriteLine(tabuada + " x " + i + " = " + resultadoMultiplicacao);
-                }
-
-                Console.ReadLine();
-                Console.BackgroundColor = ConsoleColor.Black;
-            }
-
-            static void VisualizarOperacoesRealizadas()
-            {
-                for (int i = 0; i < descricoesOperacao.Length; i++)
-                {
-                    if (descricoesOperacao[i] != null)
-                        Console.WriteLine(descricoesOperacao[i]);
-                }
-
-                Console.ReadLine();
-            }
-
-            static decimal ObterValorDecimal(string mensagem)
-            {
-                Console.WriteLine();
-
-                Console.Write(mensagem);
-
-                decimal numero = Convert.ToDecimal(Console.ReadLine());
-
-                return numero;
-            }
-
-            static void RegistrarCalculo(decimal primeiroNumero, string sinalOperacao, decimal segundoNumero, decimal resultado)
-            {
-                descricoesOperacao[quantidadeOperacoesRealizadas] =
-                        primeiroNumero + " " + sinalOperacao + " " + segundoNumero + " = " + resultado;
-
-                quantidadeOperacoesRealizadas++;
-            }
-
-            static void RealizarOperacao(string operacao)
-            {
-                decimal primeiroNumero = ObterValorDecimal("Digite o primeiro número: ");
-
-                decimal segundoNumero = ObterValorDecimal("Digite o segundo número: ");
-
-                decimal resultado = ObterResultado(operacao, primeiroNumero, ref segundoNumero);
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                ApresentarMensagem($"O resultado da operação é: {resultado}", "SUCESSO");
-                
-                Console.ResetColor();
-
-                string sinalOperacao = ObterSinalOperacao(operacao);
-
-                RegistrarCalculo(primeiroNumero, sinalOperacao, segundoNumero, resultado);
-            }
-
-            static string ObterSinalOperacao(string operacao)
-            {
-                string sinalOperacao = "";
-
-                switch (operacao)
-                {
-                    case "1": sinalOperacao = "+"; break;
-                    case "2": sinalOperacao = "-"; break;
-                    case "3": sinalOperacao = "*"; break;
-                    case "4": sinalOperacao = "/"; break;
-
-                    default:
-                        break;
-                }
-
-                return sinalOperacao;
-            }
-
-            static decimal ObterResultado(string operacao, decimal primeiroNumero, ref decimal segundoNumero)
-            {
-                decimal resultado = 0;
-
-                switch (operacao)
-                {
-                    case "1": resultado = primeiroNumero + segundoNumero; break;
-                    case "2": resultado = primeiroNumero - segundoNumero; break;
-                    case "3": resultado = primeiroNumero * segundoNumero; break;
-                    case "4":
-                        {
-                            while (segundoNumero == 0)
-                            {
-                                ApresentarMensagem("Segundo número não pode ser ZERO, tente novamente", "ERRO");
-
-                                Console.Write("Digite o segundo número: ");
-
-                                segundoNumero = Convert.ToInt32(Console.ReadLine());
-                            }
-
-                            resultado = primeiroNumero / segundoNumero;
-                            break;
-                        }
-
-                    default:
-                        break;
-                }
-
-                return Math.Round(resultado, 2);
-            }
-
+                // Lê a opção escolhida pelo usuário
+                Console.Write("Digite sua opção (1/2/3/4/5/6/0): ");
+                string opcaoOperacao = Console.ReadLine();                
             #endregion
-     }
+
+            #region opcoes do menu             
+            switch (opcaoOperacao)
+            {
+
+                case "1":
+                    resultadoOperacao = numero1 + numero2;
+                    break;
+
+                case "2":
+                    resultadoOperacao = numero1 - numero2;
+                    break;
+
+                case "3":
+                    resultadoOperacao = numero1 * numero2;
+                    break;
+
+                case "4":
+                    {
+                        while (numero2 == 0)
+                        {
+                            Console.WriteLine("Segundo número não pode ser zero, tente de novo:");
+                            Console.WriteLine();
+                            Console.WriteLine("Digite o segundo número:");
+
+                            numero2 = double.Parse(Console.ReadLine());
+                        }
+                        resultadoOperacao = numero1 / numero2;
+                        break;
+                    }
+
+                default:
+                    Console.WriteLine("mensagem...");
+                    break;
+            }                   
+            #endregion
+
+        }
+    } 
 }
+            
+            
+
+
+
